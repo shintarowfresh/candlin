e39c3651a60b1b385e4d9eb916ea518c04614dc4
阿部信太郎 <shintarowfresh@gmail.com>
2019-04-02 17:39:17 +0900

    sassが使えるプラグイン

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
