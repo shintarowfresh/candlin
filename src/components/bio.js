47b0a78b2e85dcc5aecada878be04e2a234c619e
阿部信太郎 <shintarowfresh@gmail.com>
2019-04-02 20:27:31 +0900

    プロフィール書き換え。

--- src/components/bio.js
+++ src/components/bio.js
@@ -31,11 +31,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> （ライフスタイルブロガー）Camp and UDON is everything.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Twitterをフォロー
               </a>
             </p>
           </div>
