2463ccd1ef77062d38028afb33dcad699f72d8f2
阿部信太郎 <shintarowfresh@gmail.com>
2019-04-02 17:04:53 +0900

    スターター。

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,7 +38,7 @@ exports.createPages = ({ graphql, actions }) => {
       const next = index === 0 ? null : posts[index - 1].node
 
       createPage({
-        path: post.node.fields.slug,
+        path: `posts${post.node.fields.slug}`,
         component: blogPost,
         context: {
           slug: post.node.fields.slug,
@@ -47,8 +47,6 @@ exports.createPages = ({ graphql, actions }) => {
         },
       })
     })
-
-    return null
   })
 }
 
